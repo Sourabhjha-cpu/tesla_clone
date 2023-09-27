360f1f15233c8544ac2e4a3bb56d466fac579efe
Sourabh Jha <jhasourabh674@gmail.com>
2023-09-27 21:47:10 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,38 @@
+
+
+
+
+
+
+var tl= gsap.timeline()
+
+tl.from("#nav-prt1",{
+     y:-100,
+     duration:1,
+     delay:1,
+     opacity:0
+})
+tl.from("#nav-prt2",{
+    y:-100,
+    duration:1,
+    opacity:0
+    
+})
+tl.from("#nav-prt3",{
+    y:-100,
+    duration:1,
+    opacity:0
+    
+})
+tl.from("#page1 h1",{
+    y:100,
+    opacity:0
+})
+tl.from("#page1 h4",{
+    y:100,
+    opacity:0
+})
+tl.from("#page1 button",{
+    x:-400,
+    
+})
